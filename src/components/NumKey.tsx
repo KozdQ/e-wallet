ca864e69c4f3e8233857b1602ff4b2d4c5aadb78
LAP15863 <dang.hbaokody@gmail.com>
2022-09-28 13:38:19 +0700

    topup

--- src/components/NumKey.tsx
+++ src/components/NumKey.tsx
@@ -3,7 +3,15 @@ import PropTypes from "prop-types";
 
 function NumKey(props: any) {
     return (
-        <div className={"font-bold text-xl w-1/3 h-full rounded-xs flex justify-center items-center " + props.extraStyle}>
+        <div
+            className={"font-bold text-xl w-1/3 h-full rounded-xs flex justify-center items-center " + props.extraStyle}
+            onClick={(event) => {
+                let element = event.currentTarget
+                element.style.border = "7px solid #f9efe5"
+                setTimeout(function () {
+                    element.style.border = ""
+                }, 200)
+            }}>
             {props.name}
         </div>
     )
